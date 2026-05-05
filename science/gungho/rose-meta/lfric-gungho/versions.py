f1f0ddb3e9d4dc995f8381406d8d7b6268d82342
Mohit Dalvi <mohit.dalvi@metoffice.gov.uk>
2026-05-05 11:13:42 +0100

    First round of fixes from testing: only handle fields and settings that are defined in source code; implement upgrade macro

--- science/gungho/rose-meta/lfric-gungho/versions.py
+++ science/gungho/rose-meta/lfric-gungho/versions.py
@@ -31,3 +31,46 @@ class vnXX_txxx(MacroUpgrade):
         # Add settings
         return config, self.reports
 """
+
+class vn3.1_t474(MacroUpgrade):
+    # Upgrade macro for #474 by Mohit Dalvi
+    # Add namelist and related items for Nudging functionality
+
+    BEFORE_TAG = "vn3.1"
+    AFTER_TAG = "vn3.1_t474"
+
+    def upgrade(self, config, meta_config=None):
+        # Add settings - new items in files and multires_coupling
+        self.add_setting( config,
+            ["namelist:files", "nudging_directory"],"''")
+        self.add_setting( config,
+            ["namelist:files", "nudging_filename"],"''")
+
+        self.add_setting( config,
+            ["namelist:multires_coupling", "coarse_nudging"], ".false," )
+        self.add_setting( config,
+            ["namelist:multires_coupling", "nudging_mesh_name"], "''")
+
+        # Add new nudging namelist 
+        # Append after 'multires_coupling' in configuration.nml
+        source = self.get_setting_value( config,
+                ["file:configuration.nml","source"] )
+        source = re.sub(r'namelist:multires_coupling',
+                 r'namelist:multires_coupling' + '\n' + ' (namelist:nudging)',
+                 source)
+        self.change_setting_value( config,
+                ["file:configuration.nml","source"], source )
+
+        self.add_setting(config, ["namelist:nudging"])
+        self.add_setting(config, ["namelist:nudging","nudge_data_levels"], 0 )
+        self.add_setting(config,
+                         ["namelist:nudging","nudging_level_bottom"], 0 )
+        self.add_setting(config,
+                         ["namelist:nudging","nudging_level_top"], 0 )
+        self.add_setting(config, ["namelist:nudging","nudge_source"], "''" )
+        self.add_setting(config,
+                         ["namelist:nudging","nudging_width_bottom"], 0 )
+        self.add_setting(config,
+                         ["namelist:nudging","nudging_width_top"], 0 )
+
+        return config, self.reports
